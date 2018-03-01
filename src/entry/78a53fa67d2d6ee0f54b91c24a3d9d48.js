2078b56ec78b4a832ca72f068360071f3a076b85
Changeden <changeden520@gmail.com>
2018-03-01 15:45:14 +0800

    升级至2.0.1版本

---
+++ src/entry/78a53fa67d2d6ee0f54b91c24a3d9d48.js
@@ -0,0 +1,5 @@
+'use strict';
+import {createEntry} from '../main.js';
+import myComponent from '../module/index/index/module.vue';
+const app = createEntry(myComponent);
+// Update at Thu Mar 01 2018 15:39:57 GMT+0800 (CST)
